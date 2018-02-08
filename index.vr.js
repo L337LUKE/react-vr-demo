5aad237e7bbfc2e2fef1db743ee6fa7b1d259ec8
Mikaayel Rehman <mika@Mikaayels-iMac.local>
2018-02-08 20:36:22 +0000

    added valid keys to readme, mapped tooltips from json

--- index.vr.js
+++ index.vr.js
@@ -5,36 +5,56 @@ import {
 	Pano,
 	Text,
 	View,
+	VrButton
 } from 'react-vr';
 
 export default class react_vr_demo extends React.Component {
 	static defaultProps = {
 		sourceData: 'sourceData.json'
 	};
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			rooms: null
+		};
+	}
+
 	componentDidMount() {
 		fetch(asset(this.props.sourceData).uri)
 			.then(response => response.json())
-			.then(responseData => console.log(responseData.rooms))
+			.then(responseData => this.setState({
+				rooms: responseData.rooms
+			}))
+	}
+
+	renderTooltips() {
+		let data = this.state.rooms[0].tooltips;
+		console.log(data);
+		return data.map((x) => {
+			return (
+				<View>
+					<VrButton style={{
+						backgroundColor: '#777879',
+						layoutOrigin: [.5, .5, 0],
+						position: 'absolute',
+						transform: [
+							{ rotateY: x.rotateY },
+							{ translate: [0, 0, -3] }
+						]
+					}}>
+						<Text>{x.text}</Text>
+					</VrButton>
+				</View>
+			)
+		})
 	}
 
 	render() {
 		return (
 			<View>
 				<Pano source={asset('chess-world.jpg')} />
-				<Text
-					style={{
-						backgroundColor: '#777879',
-						fontSize: 0.8,
-						fontWeight: '400',
-						layoutOrigin: [0.5, 0.5],
-						paddingLeft: 0.2,
-						paddingRight: 0.2,
-						textAlign: 'center',
-						textAlignVertical: 'center',
-						transform: [{ translate: [0, 0, -3] }],
-					}}>
-					hello
-				</Text>
+				{this.state.rooms && this.renderTooltips()}
 			</View>
 		);
 	}
